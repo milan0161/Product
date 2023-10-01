f2825e90723e7244f74d5503908bb19196799844
Milan Stanisavljevic <stanisavljevic07@gmail.com>
2023-10-01 11:49:40 +0200

    inital commit

---
+++ Domain/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int PerfumeId { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
