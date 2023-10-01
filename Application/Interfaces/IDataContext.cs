df49dd8c7f39a02791cbf4980c6af242fa2911b5
Milan Stanisavljevic <stanisavljevic07@gmail.com>
2023-10-01 12:41:02 +0200

    db context configuration

---
+++ Application/Interfaces/IDataContext.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces
+{
+    public interface IDataContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
